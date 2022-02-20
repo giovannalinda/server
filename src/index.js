59a6644a394b66024792839319e2e7ff8c3bf07d
Giovanna Linda <eugiovannasouza@gmail.com>
2022-02-20 13:21:51 -0300

    create server http

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const http = require('http')
+
+const users = require('./mocks/users')
+
+const server = http.createServer((request, response) => {
+  console.log(`Request method ${request.method} | Endpoint ${request.url}`)
+
+  if (request.url === '/users' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(users))
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' })
+    response.end(`Cannot ${request.method} ${request.url}`)
+  }
+})
+
+server.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Giovanna',
+  },
+  {
+    id: 2,
+    name: 'Gabriel',
+  },
+  {
+    id: 3,
+    name: 'John',
+  },
+]
