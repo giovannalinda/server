715b5e640ccd82dfbb5b8e64e11c43fb8612be7e
Giovanna Linda <eugiovannasouza@gmail.com>
2022-02-23 13:59:14 -0300

    update route

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const users = require('../mocks/users')
+let users = require('../mocks/users')
 
 module.exports = {
   listUsers(request, response) {
@@ -28,7 +28,7 @@ module.exports = {
 
   createUser(request, response) {
     const { body } = request
-    
+
     const lastUserId = users[users.length - 1].id
     const newUser = {
       id: lastUserId + 1,
@@ -38,5 +38,31 @@ module.exports = {
     users.push(newUser)
 
     response.send(200, newUser)
+  },
+
+  updateUser(request, response) {
+    let { id } = request.params
+    const { name } = request.body
+
+    id = Number(id)
+
+    const userExists = users.find((user) => user.id === id) 
+
+    if (!userExists) {
+      return response.send(400, { error: 'User not found!' })
+    }
+
+    users = users.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          name
+        }
+      }
+
+      return user
+    })
+
+    response.send(200, { id, name })
   }
 }
