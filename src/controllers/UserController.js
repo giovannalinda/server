2c93deb12a8b564da30d5204a155b3a021e18313
Giovanna Linda <eugiovannasouza@gmail.com>
2022-02-23 13:39:19 -0300

    Creating the bodyParser helper

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -27,24 +27,16 @@ module.exports = {
   },
 
   createUser(request, response) {
-    let body = ''
+    const { body } = request
+    
+    const lastUserId = users[users.length - 1].id
+    const newUser = {
+      id: lastUserId + 1,
+      name: body.name,
+    }
 
-    request.on('data', (chunk) => {
-      body += chunk
-    })
+    users.push(newUser)
 
-    request.on('end', () => {
-      body = JSON.parse(body)
-
-      const lastUserId = users[users.length - 1].id
-      const newUser = {
-        id: lastUserId + 1,
-        name: body.name,
-      }
-
-      users.push(newUser)
-
-      response.send(200, newUser)
-    })
+    response.send(200, newUser)
   }
 }
